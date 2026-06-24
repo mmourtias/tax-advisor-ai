cb2b3b1c40d37a8fe3a8b6cc36caa1b4a7da3e69
mmourtias <mmourtias2002@gmail.com>
2026-06-24 14:39:53 +0300

    changes in tests

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,14 +1,11 @@
 from fastapi.testclient import TestClient
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import patch, AsyncMock
 from main import app
 
 client = TestClient(app)
 
 def test_valid_tax_advice():
-    # Αντί να καλέσει το Groq, επιστρέφει αυτό
-    with patch[MagicMock | AsyncMock]("router.get_tax_advice") as mock_llm:
-        mock_llm.return_value = "This is a fake tax advice response"
-        
+    with patch("router.get_tax_advice", return_value="This is a fake tax advice response"):
         response = client.post("/tax-advice", json={
             "country": "Greece",
             "income": 15000,
